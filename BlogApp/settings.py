211d21f74ddeaa6e33ee44eadb50a90422733765
Aman saxena <asaxena7531@gmail.com>
2021-07-13 14:21:52 +0530

    using media query in index.html

--- BlogApp/settings.py
+++ BlogApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_xm(67922e0g#+!7&pk+y%hb&j7s@i*gj*ff#s6%2qklkef=u-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.43.186','127.0.0.1']
 
 
 # Application definition
